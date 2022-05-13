6b9f90ca466c88a8e90b5579ff3efb3d9b6855b6
Kento Ueda <beatweapon7th@gmail.com>
2022-05-13 14:45:08 +0900

    キーワードサービスを実装

---
+++ src/app/core/services/keyword.service.ts
@@ -0,0 +1,128 @@
+import { Injectable } from '@angular/core';
+import { Subscription } from 'rxjs';
+import {
+  Firestore,
+  collectionData,
+  collection,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+  query,
+  orderBy,
+  serverTimestamp,
+} from '@angular/fire/firestore';
+
+export interface KeywordWithId extends Keyword {
+  id: string;
+}
+
+export type Keyword = {
+  keyword: string;
+  tags?: string[];
+  url?: string;
+  createDate?: any;
+  updateDate: any;
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class KeywordService {
+  private keywordSubscription: Subscription | null = null;
+  private keywordCollectionPath = '';
+  public keywords: KeywordWithId[] = [];
+
+  constructor(private firestore: Firestore) {}
+
+  /**
+   * 受け取ったユーザーIDのキーワードコレクションの監視を開始する
+   * @param userId データ取得対象のuserId
+   */
+  subscribeCollection(userId: string) {
+    this.crear();
+
+    this.setKeywordCollectionPath(userId);
+
+    const keywordCollection = collection(
+      this.firestore,
+      this.keywordCollectionPath
+    );
+
+    const q = query(keywordCollection, orderBy('createDate', 'desc'));
+
+    this.keywordSubscription = collectionData(q, { idField: 'id' }).subscribe(
+      (assets) => {
+        this.keywords = assets as KeywordWithId[];
+      }
+    );
+  }
+
+  /**
+   * 参照するコレクションのパスを設定する
+   * @param userId データ取得対象のuserId
+   */
+  setKeywordCollectionPath(userId: string) {
+    this.keywordCollectionPath = `users/${userId}/keywords`;
+  }
+
+  /**
+   * キーワード新規追加
+   * @param userId 追加対象のユーザーID
+   * @param keyword 新規に追加する文字列
+   * @param tags キーワードに追加するタグ
+   * @param url キーワードにまつわるURL
+   */
+  async addKeyword(keyword: string, tags: string[] = [], url: string = '') {
+    const newKeyWord: Keyword = {
+      keyword,
+      tags,
+      url,
+      createDate: serverTimestamp(),
+      updateDate: serverTimestamp(),
+    };
+
+    const keywordCollection = collection(
+      this.firestore,
+      this.keywordCollectionPath
+    );
+
+    await addDoc(keywordCollection, newKeyWord);
+  }
+
+  /**
+   * キーワード情報更新
+   * @param keywordDataWithId 更新対象のデータ
+   */
+  async updateKeywordData(keywordWithId: KeywordWithId) {
+    const newData: Keyword = {
+      keyword: keywordWithId.keyword,
+      tags: keywordWithId.tags || [],
+      url: keywordWithId.url || '',
+      updateDate: serverTimestamp(),
+    };
+
+    await updateDoc(
+      doc(this.firestore, this.keywordCollectionPath, keywordWithId.id),
+      newData
+    );
+  }
+
+  /**
+   * キーワード情報削除
+   * @param userId ユーザーのid
+   * @param keywordId キーワードデータのid
+   */
+  deleteKeywordData(keywordId: string) {
+    deleteDoc(doc(this.firestore, this.keywordCollectionPath, keywordId));
+  }
+
+  crear() {
+    if (this.keywordSubscription) {
+      this.keywordSubscription.unsubscribe();
+    }
+
+    this.keywordCollectionPath = '';
+    this.keywords = [];
+  }
+}
