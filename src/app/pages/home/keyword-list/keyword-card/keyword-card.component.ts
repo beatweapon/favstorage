f63f5a49ce2ed61bb498e18d84923c9506025c99
Kento Ueda <beatweapon7th@gmail.com>
2022-05-28 13:14:57 +0900

    カードの閲覧と編集を切り替えた際に高さの変化がアニメーションするように対応

--- src/app/pages/home/keyword-list/keyword-card/keyword-card.component.ts
+++ src/app/pages/home/keyword-list/keyword-card/keyword-card.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 import { KeywordWithId } from '@/core/services/keyword.service';
 
 @Component({
@@ -12,6 +20,9 @@ export class KeywordCardComponent implements OnInit {
   @Input() isEditable: boolean = false;
   @Output() toggleSearchWord: EventEmitter<any> = new EventEmitter();
   isEditMode = false;
+  height = 'auto';
+
+  @ViewChild('card') card!: ElementRef;
 
   constructor() {}
 
@@ -19,9 +30,38 @@ export class KeywordCardComponent implements OnInit {
 
   openEditor() {
     this.isEditMode = true;
+
+    this.changeHeight();
   }
 
   closeEditor() {
     this.isEditMode = false;
+
+    this.changeHeight();
+  }
+
+  changeHeight() {
+    // 初期値がautoの可能性があるので最初に高さを取って設定する
+    this.height = this.getCardHeight();
+
+    setTimeout(() => {
+      this.height = this.getCardHeight();
+
+      // アニメーション終了後はautoに戻さないと高さが固定されてしまいウィンドウ幅の変更に追従できない
+      // 100msはcssのtransitionの時間
+      setTimeout(() => {
+        this.height = 'auto';
+      }, 100);
+    });
+  }
+
+  getCardHeight() {
+    if (!this.card) return 'auto';
+
+    const cardMargin = 16;
+
+    const height = this.card.nativeElement.offsetHeight + cardMargin;
+
+    return height + 'px';
   }
 }
